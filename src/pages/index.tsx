d78e78ce50a0fe1d17c2413ae8de7ba917900596
manonab <manon.abel-coindoz@hostnfly.com>
2023-11-03 14:27:20 +0100

    added first part of the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import "animate.css";
 import { NextPage } from "next";
+import Eye from "../../public/static/assets/animation.png";
+import Image from "next/image";
 
 const Home: NextPage = () => {
 
   return (
-    <div className="w-screen h-screen">
-
+    <div className="mx-auto top-[90px] relative">
+      <Image
+        src={Eye}
+        alt="Image"
+        width={400}
+        height={400}
+        className="animate__animated animate__fadeInUpBig  animate__slow rounded mx-auto my-auto"
+      />
     </div>
   );
-};
+}
 export default Home;
