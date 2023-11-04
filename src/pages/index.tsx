32bfc394f01788183fbf42712bb84591cac59cc0
manonab <manon.abel-coindoz@hostnfly.com>
2023-11-04 10:12:30 +0100

    added some delay for the last animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,19 +68,20 @@ const Home: NextPage = () => {
       {/* En attendant d'avoir la fin de l'animation. */}
       {currentImage === images.length - 1 && (
         <motion.div
-          className="flex justify-between items-start z-10 h-auto animate__animated animate__fadeInUpBig inset-0 absolute -top-20"
+          className="flex justify-between items-start z-10 h-auto inset-0 absolute -top-20"
           initial="hidden"
           animate="visible"
           variants={sectionVariants}
         >
-          <Image src={Images.mosaic} alt="mosaic image" className="w-[310px] h-[425px]" />
-          <div className="relative">
+          <Image src={Images.mosaic} alt="mosaic image" className="w-[310px] h-[425px] animate__animated animate__fadeInUpBig animate__delay-0.2s" />
+          <div className="relative animate__animated animate__fadeInUpBig animate__delay-1.3s">
             <Image src={Images.bear} alt="bear image" className="w-[550px] h-[600px] mt-[80px]" />
             <p className="text-mainColor absolute top-[50%] left-[20%] transform translate(-50%, -50%) font-neueMedium text-[80px]">Welcome.</p>
-            <button className="bg-grayBlack mx-auto self-center absolute top-[75%] py-2.5 px-10 w-[163px] font-inter text-xs uppercase text-white font-bold	rounded-[50px] left-[40%] transform translate(-50%, -50%)">See more</button>
+            <button className="bg-grayBlack mx-auto self-center absolute top-[75%] py-2.5 px-10 w-[163px] font-inter text-xs uppercase text-white font-bold	rounded-[50px] left-[40%] transform translate(-50%, -50%) animate__delay-0.2s">See more</button>
           </div>
-          <Image src={Images.capiche} alt="capiche image" className="w-[300px] h-[380px] opacity-[0.9] mt-[300px]" />
+          <Image src={Images.capiche} alt="capiche image" className="w-[300px] h-[380px] opacity-[0.9] mt-[300px] animate__animated animate__fadeInUpBig animate__delay-1.5s" />
         </motion.div>
+
       )}
 
     </div>
